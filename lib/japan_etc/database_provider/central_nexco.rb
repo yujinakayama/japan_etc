1d24a01b944a202a00fbb347d49981fb75671418
Yuji Nakayama <nkymyj@gmail.com>
2019-06-06 04:00:36 +0900

    Support route name in 広島高速

--- lib/japan_etc/database_provider/central_nexco.rb
+++ lib/japan_etc/database_provider/central_nexco.rb
@@ -2,6 +2,7 @@
 
 require 'japan_etc/database_provider/base'
 require 'japan_etc/tollbooth'
+require 'japan_etc/util'
 require 'faraday'
 require 'pdf-reader'
 
@@ -9,6 +10,8 @@ module JapanETC
   module DatabaseProvider
     # https://highwaypost.c-nexco.co.jp/faq/etc/use/50.html
     class CentralNEXCO < Base
+      include Util
+
       URL = 'https://highwaypost.c-nexco.co.jp/faq/etc/use/documents/190423-2etcriyoukanouic.pdf'
 
       WHITESPACE = /[\s　]/.freeze
@@ -37,7 +40,7 @@ module JapanETC
 
       IDENTIFIER_PATTERN = /\b(\d{2})\s+(\d{3})\b/.freeze
 
-      attr_reader :current_road_name, :current_tollbooth_name
+      attr_reader :current_road_name, :current_route_name, :current_tollbooth_name
 
       def fetch_tollbooths
         lines.flat_map { |line| parse_line(line) }.compact
@@ -47,7 +50,10 @@ module JapanETC
         match = line.match(TOLLBOOTH_LINE_PATTERN)
         return unless match
 
-        @current_road_name = match[:road_name] if match[:road_name]
+        if match[:road_name]
+          @current_road_name, @current_route_name =
+            extract_route_name_from_road_name(match[:road_name])
+        end
 
         @current_tollbooth_name = match[:tollbooth_name] if match[:tollbooth_name]
 
@@ -58,10 +64,17 @@ module JapanETC
             road_number: identifier.first,
             tollbooth_number: identifier.last,
             road_name: current_road_name,
+            route_name: current_route_name,
             name: current_tollbooth_name,
             note: match[:note]
           )
         end
+      end
+
+      def extract_route_name_from_road_name(road_name)
+        road_name = normalize(road_name)
+        match = road_name.match(/\A(?<road_name>.+?)(?<route_name>\d+号.+)?\z/)
+        [match[:road_name], match[:route_name]]
       end
 
       def lines
